f4c48ae9ad6907471959389bb2150d5e4791ac5d
ollieshmollie <oliverduncan@icloud.com>
2016-12-09 11:48:09 -0600

    Design Code, Guess, and Key objects

---
+++ guess.js
@@ -0,0 +1,61 @@
+var colors = {
+  red: "rgb(255, 0, 0)",
+  green: "rgb(0, 255, 0)",
+  blue: "rgb(0, 0, 255)",
+  yellow: "rgb(255, 255, 0)",
+  black: "rgb(0, 0, 0)",
+  white: "rgb(255, 255, 255)"
+};
+
+function Code() {
+  this.array = (function() {
+    var code = [];
+    var colorArray = Object.keys(colors);
+    for (var i = 0; i < 4; i++) {
+      var randomNumber = Math.random();
+      var colorIndex = Math.floor(randomNumber * colorArray.length);
+      var color = colorArray[colorIndex];
+      code.push(color);
+    }
+    return code;
+  })();
+}
+
+function Guess(colorArray) {
+  this.array = colorArray;
+}
+
+function Key(guess, code) {
+  this.array = (function() {
+    var positionMatches = function() {
+      var count = 0;
+      for (var i = 0; i < code.array.length; i++) {
+        if (guess.array[i] === code.array[i]) count++;
+      }
+      return count;
+    }
+
+    var colorMatches = function() {
+      var count = 0;
+      for (var i = 0; i < guess.array.length; i++) {
+        if (guess.array[i] !== code.array[i]) {
+          for (var j = i+1; j < guess.array.length; j++) {
+            if (guess.array[i] === code.array[j]) count++;
+          }
+        }
+      }
+      return count;
+    }
+    
+    var key = [];
+    
+    for (var i = 0; i < positionMatches(); i++) {
+      key.push('black');
+    }
+    for (var i = 0; i < colorMatches(); i++) {
+      key.push('white');
+    }
+    
+    return key;
+  })();
+}
