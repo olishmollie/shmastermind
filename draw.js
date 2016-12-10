32b3ba6cfc48a89fc6e36b9da95191931bc7a889
ollieshmollie <oliverduncan@icloud.com>
2016-12-09 21:33:08 -0600

    Layout main board

--- draw.js
+++ draw.js
@@ -1,10 +1,14 @@
 var canvas = document.getElementById("board");
 var ctx = canvas.getContext("2d");
+var $gutter = $("#gutter");
+
+// Gutter width depends on canvas
+$gutter.width(canvas.width);
 
 var code = new Code();
 
 var startX = 40;
-var y = 40;
+var y = 25;
 var r = 20;
 
 ctx.strokeStyle = colors['black'];
@@ -12,7 +16,7 @@ ctx.lineWidth = 2;
 
 // Draw code background
 ctx.fillStyle = "rgb(200, 200, 200)";
-ctx.fillRect(0, 0, canvas.width, 80);
+ctx.fillRect(0, 0, canvas.width, 50);
 
 // Draw code
 for (var i = 0; i < code.array.length; i++) {
